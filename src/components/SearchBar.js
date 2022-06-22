e8c778f0bc3e830361d67afbd9301c3efdd40768
Mason Wagoner <mgwagoner@gmail.com>
2022-06-22 15:26:44 -0400

    updated searchbar functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,12 @@ function SearchBar(props) {
 
   return (
     <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-      <input type='text' placeholder='Enter a search term here'
-      onChange={(e) => setSearchTerm(e.target.value)} />
-      <input type='submit' />
+        <input type='text' placeholder='Enter a search term here'
+            onChange={
+                (e) => props.handleSearch(e, e.target.value)
+            } />
+        <input type='submit'  />
+        <input type='reset' value='Clear' />
     </form>
   );
 }
